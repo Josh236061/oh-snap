f2d81f7a2bb36e8496d67f597d2c804938425be0
Josh236061 <71394743+Josh236061@users.noreply.github.com>
2022-12-21 20:53:43 -0500

    Finalized Lesson 1 Code

---
+++ photo-port/src/components/About/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import About from './components/About';
+
+function App() {
+
+  return (
+    <div>
+      <main>
+        <About></About>
+      </main>
+    </div>
+  );
+}
+
+export default App;
