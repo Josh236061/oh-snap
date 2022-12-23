742406318e0608bfd07c010c5d286f8a9a7eea96
Josh236061 <71394743+Josh236061@users.noreply.github.com>
2022-12-22 19:53:52 -0500

    Finalized Lesson 2 Code

---
+++ photo-port/src/components/About/__tests__/index.test.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import About from '..';
+import '@testing-library/jest-dom/extend-expect';
+
+describe('About component', () => {
+    // First Test
+    it('renders', () => {
+      render(<About />);
+    });
+  
+    // Second Test
+  })
